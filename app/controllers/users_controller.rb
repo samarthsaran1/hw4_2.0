9853f3b41bd952896af9d5d57f83600582934757
Samarth Saran <samarth.saran@kellogg.northwestern.edu>
2024-06-05 09:29:32 +0000

    commit 12

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,8 @@ class UsersController < ApplicationController
       @user["email"] = params["email"]
       @user["password"] = BCrypt::Password.create(params["password"])
       @user.save
-      redirect_to "/login"
       flash["notice"] = "Account Successfully Created"
+      redirect_to "/login"
     end
   end
 end
